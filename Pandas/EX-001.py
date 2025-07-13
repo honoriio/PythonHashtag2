ce60e6c57350e6a1993fe22e856a62ef8a3bd5ca
Diego Honório <diegohonoriiio@gmail.com>
2025-07-13 15:20:50 -0300

    bug eliminado
    Bug da key ID Cliente e ID Vendas eliminado.

--- Pandas/EX-001.py
+++ Pandas/EX-001.py
@@ -1,30 +1,27 @@
 import pandas as pd
 from IPython.display import display
 
-vendas_df = pd.read_csv(r'Pandas/Contoso - Vendas - 2017.csv', sep=';')  # OK
-produtos_df = pd.read_csv(r'Pandas/Contoso - Cadastro Produtos.csv', sep=';', encoding='latin1') #ok
-lojas_df = pd.read_csv(r'Pandas/Contoso - Lojas.csv', sep=';', encoding='latin1') #ok
-clientes_df = pd.read_csv(r'Pandas/Contoso - Clientes.csv', sep=';', encoding='latin1') #ok
+# Leitura dos arquivos
+vendas_df = pd.read_csv(r'Pandas/Contoso - Vendas - 2017.csv', sep=';')
+produtos_df = pd.read_csv(r'Pandas/Contoso - Cadastro Produtos.csv', sep=';', encoding='latin1')
+lojas_df = pd.read_csv(r'Pandas/Contoso - Lojas.csv', sep=';', encoding='latin1')
+clientes_df = pd.read_csv(r'Pandas/Contoso - Clientes.csv', sep=';', encoding='latin1')
 
-# Essa parte do codigo retira o caracter indesejado no inicio das colunas
+# Limpeza de caracteres indesejados nos nomes das colunas
 produtos_df.columns = produtos_df.columns.str.strip().str.replace('ÿ', '')
 lojas_df.columns = lojas_df.columns.str.strip().str.replace('ÿ', '')
 clientes_df.columns = clientes_df.columns.str.strip().str.replace('ÿ', '')
 
-
-# essa parte do codigo coleta somente as colunas que preciso usar para fazer o marge entre as tabelas. 
-vendas_df = vendas_df[['ID Produto']]
+# Seleção das colunas necessárias nos DataFrames auxiliares
 produtos_df = produtos_df[['ID Produto', 'Nome do Produto']]
 lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
 clientes_df = clientes_df[['ID Cliente', 'E-mail']]
 
-
-# essa parte esta com erro, o mesmo dis que a Key esta incorreta, no caso as keys de Lojas_df e Clientes_df
+# Realizando os merges
 vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
 vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
 vendas_df = vendas_df.merge(clientes_df, on='ID Cliente')
 
-
-
-display(vendas_df)
-
+# Exibição
+display(lojas_df)
+display(clientes_df)
